3f57456d991281a7ae74654b4577ae467b923113
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-19 23:00:41 +0530

    add app.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (_, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Orvexa backend running"
+    });
+});
+
+export default app;
