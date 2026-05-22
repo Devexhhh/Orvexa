cde8e4d6bff87d1a22baa3e2834ca59f741cdb6e
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-22 22:34:16 +0530

    added parser util

---
+++ backend/src/ws/utils/parser.ts
@@ -0,0 +1,17 @@
+import { SocketMessage } from "../types/event.types";
+
+export function parseSocketMessage(
+    rawMessage: string
+): SocketMessage | null {
+
+    try {
+
+        return JSON.parse(rawMessage);
+
+    } catch (error) {
+
+        return null;
+
+    }
+
+}
