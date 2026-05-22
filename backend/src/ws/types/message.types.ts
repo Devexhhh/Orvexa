d4cdcc1537970c29adf8178951c07c9ddc391f10
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-22 22:34:44 +0530

    added message types

---
+++ backend/src/ws/types/message.types.ts
@@ -0,0 +1,31 @@
+export enum MessageType {
+    TEXT = "TEXT",
+
+    IMAGE = "IMAGE",
+
+    FILE = "FILE",
+
+    LINK = "LINK",
+
+    SYSTEM = "SYSTEM"
+}
+
+export interface ChatMessage {
+
+    roomId: string;
+
+    senderId?: string;
+
+    type: MessageType;
+
+    content: string;
+
+    fileUrl?: string;
+
+    fileName?: string;
+
+    fileSize?: number;
+
+    createdAt?: Date;
+
+}
