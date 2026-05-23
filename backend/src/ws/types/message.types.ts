9a85ddbe679123b08d755f9e17d1409e5264f32b
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-23 23:41:10 +0530

    modified message types

--- backend/src/ws/types/message.types.ts
+++ backend/src/ws/types/message.types.ts
@@ -1,31 +1,18 @@
 export enum MessageType {
     TEXT = "TEXT",
-
     IMAGE = "IMAGE",
-
     FILE = "FILE",
-
     LINK = "LINK",
-
-    SYSTEM = "SYSTEM"
+    SYSTEM = "SYSTEM",
 }
 
 export interface ChatMessage {
-
     roomId: string;
-
     senderId?: string;
-
     type: MessageType;
-
     content: string;
-
     fileUrl?: string;
-
     fileName?: string;
-
     fileSize?: number;
-
     createdAt?: Date;
-
 }
