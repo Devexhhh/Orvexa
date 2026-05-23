38d414087da3f6afd4ea8e55ae7b7eec5e6c8bfe
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-23 23:41:22 +0530

    modified apps

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,15 +4,13 @@ import cors from "cors";
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (_, res) => {
     res.status(200).json({
         success: true,
-        message: "Orvexa backend running"
+        message: "Orvexa backend running",
     });
 });
 
