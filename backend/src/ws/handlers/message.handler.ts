6ae18175d957ddae1c8efdbff88ace7a85ad81ef
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-26 22:49:52 +0530

    add message handler

--- backend/src/ws/handlers/message.handler.ts
+++ backend/src/ws/handlers/message.handler.ts
@@ -1,49 +1,70 @@
-import { CustomWebSocket }
-    from "../types/ws.types";
+import { prisma } from "@database/prisma";
+import { createNotificationService } from "@modules/notifications/notification.service";
+import { NotificationType } from "@modules/notifications/notification.types";
+import { sendNotificationToUser } from "./notification.handler";
+import { CustomWebSocket } from "../types/ws.types";
+import { SocketEvent } from "../types/event.types";
+import { broadcastMessage } from "../utils/broadcast";
+import { createMessageService } from "@modules/messages/message.service";
+import { MessageType } from "@modules/messages/message.types";
+import { createMessageSchema } from "@modules/messages/message.schema";
 
-import {
-    ChatMessage,
-    MessageType
-}
-    from "../types/message.types";
-
-import { broadcastMessage }
-    from "../utils/broadcast";
-
-export async function handleSendMessage(
-    ws: CustomWebSocket,
-    data: ChatMessage
-) {
-
-    const messagePayload: ChatMessage = {
-
-        roomId: data.roomId,
-
-        senderId: ws.userId || "anonymous",
-
-        type: data.type || MessageType.TEXT,
-
-        content: data.content,
-
-        fileUrl: data.fileUrl,
-
-        fileName: data.fileName,
-
-        fileSize: data.fileSize,
-
-        createdAt: new Date()
-
-    };
-
-    //
-    // Future:
-    // Save to database here
-    //
+export async function handleSendMessage(ws: CustomWebSocket, data: any) {
+    const validatedData = createMessageSchema.parse(data);
+    const savedMessage = await createMessageService({
+        roomId: validatedData.roomId,
+        senderId: ws.userId!,
+        type: validatedData.type || MessageType.TEXT,
+        content: validatedData.content,
+        fileUrl: validatedData.fileUrl,
+        fileName: validatedData.fileName,
+        fileSize: validatedData.fileSize,
+    });
 
     broadcastMessage(
-        data.roomId,
-        "MESSAGE_RECEIVED",
-        messagePayload
+        validatedData.roomId,
+        SocketEvent.MESSAGE_RECEIVED,
+        savedMessage,
     );
+    //
+    // Notify room members
+    //
+    const roomMembers = await prisma.roomMember.findMany({
+        where: {
+            roomId: data.roomId,
+            userId: {
+                not: ws.userId,
+            },
+        },
+        include: {
+            user: true,
+        },
+    });
 
+    for (const member of roomMembers) {
+        const notification = await createNotificationService({
+            userId: member.userId,
+            type: NotificationType.MESSAGE,
+            title: "New Message",
+            content: savedMessage.content,
+            metadata: {
+                roomId: data.roomId,
+
+                messageId: savedMessage.id,
+            },
+        });
+
+        sendNotificationToUser(
+            member.userId,
+            notification,
+        );
+    }
+    ws.send(
+        JSON.stringify({
+            event: SocketEvent.MESSAGE_DELIVERED,
+            data: {
+                messageId: savedMessage.id,
+            },
+        }),
+    );
 }
