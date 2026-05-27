7527c2ff88e3ddd5317d35e42cc14a3b9a9bcb28
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-27 22:23:18 +0530

    add errorMiddleware

---
+++ backend/src/shared/errors/errorMiddleware.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "./AppError";
+
+export function errorMiddleware(
+    error: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    //
+    // Known app error
+    //
+    if (error instanceof AppError) {
+        return res.status(error.statusCode).json({
+            success: false,
+            message: error.message,
+        });
+    }
+
+    //
+    // Unknown error
+    //
+    console.error(error);
+
+    return res.status(500).json({
+        success: false,
+        message: "Internal server error",
+    });
+}
