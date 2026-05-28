149e1fae07a45037c6ac1e6e1a44a8e013ce3a15
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-28 22:17:46 +0530

    refactor: modify auth controller

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,24 +1,33 @@
 import { Request, Response } from "express";
 import { generateToken } from "./jwt";
+import { prisma } from "@database/prisma";
 
-export function googleCallback(
-    req: Request,
-    res: Response,
-) {
+export function googleCallback(req: Request, res: Response) {
     const user = req.user as any;
     const token = generateToken(user.id);
     const frontendUrl = "http://localhost:3001/auth/callback";
     return res.redirect(`${frontendUrl}?token=${token}`);
 }
 
-export function getCurrentUser(
-    req: Request,
-    res: Response,
-) {
+export async function getCurrentUser(req: Request, res: Response) {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: req.userId,
+            },
+        });
 
-    return res.status(200).json({
-        success: true,
-        user: req.user,
-    });
+        res.setHeader("Cache-Control", "no-store");
 
+        return res.status(200).json({
+            success: true,
+
+            user,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Failed to fetch user",
+        });
+    }
 }
