e0f3a583715b57b354ff9b9075fa50711969ff2c
Devexhhh <kumarsujal.codes@gmail.com>
2026-06-01 22:46:35 +0530

    feat: add server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,14 @@
 import http from "http";
 import app from "./app";
-import { WebSocketServer } from "ws";
-import { attachWebSocketHandlers } from "./ws/wsHandler";
+import * as WS from "ws";
+import { attachWebSocketHandlers } from "./socket/wsHandler";
+import { authenticateSocket } from "./socket/middleware/authSocket";
+import { connectRedis } from "@config/redis";
+import { subscribeToRoomEvents } from "socket/utils/redisSubscriber";
 
 const PORT = process.env.PORT || 3000;
-
 const server = http.createServer(app);
-
-const wss = new WebSocketServer({
+const wss = new WS.WebSocketServer({
     noServer: true,
 });
 
@@ -17,8 +18,15 @@ server.on("upgrade", (req, socket, head) => {
     try {
         const url = new URL(req.url!, `http://${req.headers.host}`);
         if (url.pathname === "/ws") {
+            const authPayload = authenticateSocket(req);
+            if (!authPayload) {
+                socket.destroy();
+                return;
+            }
             wss.handleUpgrade(req, socket, head, (ws) => {
-                wss.emit("connection", ws, req);
+                const customSocket = ws as any;
+                customSocket.userId = authPayload.userId;
+                wss.emit("connection", customSocket, req);
             });
         } else {
             socket.destroy();
@@ -28,6 +36,16 @@ server.on("upgrade", (req, socket, head) => {
     }
 });
 
-server.listen(Number(PORT), "0.0.0.0", () => {
-    console.log(`🚀 Orvexa backend running on port ${PORT}`);
-});
+async function bootstrap() {
+    await connectRedis();
+    await subscribeToRoomEvents();
+    server.listen(
+        Number(PORT),
+        "0.0.0.0",
+        () => {
+            console.log(`Server running on ${PORT}`);
+        },
+    );
+}
+
+bootstrap();
