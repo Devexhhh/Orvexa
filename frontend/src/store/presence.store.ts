1370db221d646d57863e2b7091148af0b7b64146
Devexhhh <kumarsujal.codes@gmail.com>
2026-06-12 22:09:02 +0530

    feat: add presence store

---
+++ frontend/src/store/presence.store.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+
+interface PresenceState {
+    onlineUsers: string[];
+    setOnlineUsers: (users: string[]) => void;
+    addOnlineUser: (userId: string) => void;
+    removeOnlineUser: (userId: string) => void;
+}
+
+export const usePresenceStore = create<PresenceState>((set) => ({
+    onlineUsers: [],
+
+    setOnlineUsers: (users) =>
+        set({
+            onlineUsers: users,
+        }),
+
+    addOnlineUser: (userId) =>
+        set((state) => ({
+            onlineUsers: [...new Set([...state.onlineUsers, userId])],
+        })),
+
+    removeOnlineUser: (userId) =>
+        set((state) => ({
+            onlineUsers: state.onlineUsers.filter((id) => id !== userId),
+        })),
+}));
